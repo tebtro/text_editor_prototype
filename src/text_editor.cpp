224b9e0f385d5f44f6e577009b31b181a6f9dc5e
Robert Ortner <robert.ortner.aut@gmail.com>
2019-05-17 23:32:21 +0200

    simple text editing !!!

--- src/text_editor.cpp
+++ src/text_editor.cpp
@@ -41,6 +41,30 @@ struct Game_Controller_Input {
     };
 };
 
+struct Cursor {
+    u64 line;
+    u64 offset;
+};
+
+void render_glyph(SDL_Renderer *renderer, TTF_Font *font,
+                  char ch, SDL_Color fg, SDL_Color bg,
+                  int x, int y, int glyph_width, int glyph_height) {
+    SDL_Texture *texture;
+    int width, height;
+    auto surface = TTF_RenderGlyph_Shaded(font, ch, fg, bg);
+    if (surface == nullptr) {
+        SDL_Rect rect = {(int)x * glyph_width, (int)y * glyph_height, glyph_width, glyph_height};
+        SDL_RenderFillRect(renderer, &rect);
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    assert(texture);
+    width = surface->w;
+    height = surface->h;
+    SDL_FreeSurface(surface);
+    SDL_Rect rect = {(int)x * glyph_width, (int)y * glyph_height, width, height};
+    SDL_RenderCopy(renderer, texture, nullptr, &rect);
+}
+
 int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
@@ -89,6 +113,16 @@ int main(int argc, char *argv[]) {
     ch_[1].push_back('r');
     ch_[1].push_back('!');
 
+    Cursor cursor;
+    cursor.line = ch_.size() - 1;
+    cursor.offset = ch_[cursor.line].size();
+    
+    SDL_Color fg = {255, 255, 255, 0};
+    SDL_Color bg = {0, 0, 0, 0};
+
+    SDL_Color cursor_fg = bg;
+    SDL_Color cursor_bg = fg;
+    
     b32 running = true;
     SDL_StartTextInput();
     defer {SDL_StopTextInput();};
@@ -101,68 +135,102 @@ int main(int argc, char *argv[]) {
             }
             if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
                 switch (event.key.keysym.sym) {
-                    case SDLK_ESCAPE:
+                    case SDLK_ESCAPE: {
                         running = !(event.type == SDL_KEYDOWN);
-                        break;
-                    case SDLK_UP:
-                        input.up.is_pressed = (event.type == SDL_KEYDOWN);
-                        break;
-                    case SDLK_DOWN:
-                        input.down.is_pressed = (event.type == SDL_KEYDOWN);
-                        break;
-                    case SDLK_LEFT:
-                        input.left.is_pressed = (event.type == SDL_KEYDOWN);
-                        break;
-                    case SDLK_RIGHT:
-                        input.right.is_pressed = (event.type == SDL_KEYDOWN);
-                        break;
+                    } break;
+                    case SDLK_RETURN: {
+                        if (event.type != SDL_KEYDOWN) break;
+                        if (cursor.line + 2 > ch_.size()) {
+                            ch_.push_back({});
+                            cursor.line++;
+                            cursor.offset = 0;
+                        }
+                    } break;
+                    case SDLK_BACKSPACE: {
+                        if (event.type != SDL_KEYDOWN) break;
+                        if (cursor.line == 0 && cursor.offset == 0) break;
+                        if (cursor.offset > 0) {
+                            ch_[cursor.line].erase(ch_[cursor.line].begin() + --cursor.offset);
+                            break;
+                        }
+                        if (cursor.offset == 0 && cursor.line > 0) {
+                            if (ch_[cursor.line].size() == 0) ch_.erase(ch_.begin() + cursor.line);
+                            cursor.line--;
+                            cursor.offset = ch_[cursor.line].size();
+                            // ch_[cursor.line].erase(ch_[cursor.line].begin() + --cursor.offset);
+                        }
+                    } break;
+                    case SDLK_UP: {
+                        if (event.type != SDL_KEYDOWN) break;
+                        if (cursor.line > 0) {
+                            cursor.line--;
+                            if (ch_[cursor.line].size() < cursor.offset) cursor.offset = ch_[cursor.line].size();
+                        }
+                    } break;
+                    case SDLK_DOWN: {
+                        if (event.type != SDL_KEYDOWN) break;
+                        if (cursor.line < ch_.size() - 1) {
+                            cursor.line++;
+                            if (ch_[cursor.line].size() < cursor.offset) cursor.offset = ch_[cursor.line].size();
+                        }
+                    } break;
+                    case SDLK_LEFT: {
+                        if (event.type != SDL_KEYDOWN) break;
+                        if (cursor.offset > 0) {
+                            cursor.offset--;
+                        } else if (cursor.offset == 0 && cursor.line > 0) {
+                            if (cursor.line > 0) {
+                                cursor.line--;
+                            }
+                            cursor.offset = ch_[cursor.line].size();
+                        }
+                    } break;
+                    case SDLK_RIGHT: {
+                        if (event.type != SDL_KEYDOWN) break;
+                        if (cursor.line < ch_.size()) {
+                            if (cursor.offset < ch_[cursor.line].size()) {
+                                cursor.offset++;
+                            } else if (cursor.line < ch_.size() - 1){
+                                cursor.line++;
+                                cursor.offset = 0;
+                            }
+                        }
+                    } break;
                 }
                 break;
             }
             if (event.type == SDL_TEXTINPUT) {
-                ch_[0].push_back(event.text.text[0]);
+                ch_[cursor.line].push_back(event.text.text[0]);
+                cursor.offset++;
                 break;
             }
         }
 
         // Update
-        /*
-        if (input.up.is_pressed)    text_rect.y -= 1;
-        if (input.down.is_pressed)  text_rect.y += 1;
-        if (input.left.is_pressed)  text_rect.x -= 1;
-        if (input.right.is_pressed) text_rect.x += 1;
-        */
 
         // Render
-        SDL_SetRenderTarget(renderer, nullptr);
-        SDL_Rect r = {0,0,SCREEN_WIDTH,SCREEN_HEIGHT};
-        SDL_RenderCopy(renderer, texture, nullptr, &r);
+        b32 cursor_rendered = false;
+
 
         SDL_SetRenderTarget(renderer, texture);
+        SDL_RenderClear(renderer);
         for (size_t y = 0; y < ch_.size(); ++y) {
             for (size_t x = 0; x < ch_[y].size(); ++x) {
                 const auto &ch = ch_[y][x];
-                SDL_Color fg = {255, 255, 255, 0};
-                SDL_Color bg = {0, 0, 0, 0};
-
-                SDL_Texture *texture;
-                int width, height;
-                auto surface = TTF_RenderGlyph_Shaded(font, ch, fg, bg);
-                if (surface == nullptr) {
-                    SDL_Rect rect = {(int)x * glyph_width, (int)y * glyph_height, glyph_width, glyph_height};
-                    SDL_RenderFillRect(renderer, &rect);
+                if (y == cursor.line && x == cursor.offset) {
+                    render_glyph(renderer, font, ch, bg, fg, x, y, glyph_width, glyph_height);
+                    cursor_rendered = true;
+                } else {
+                    render_glyph(renderer, font, ch, fg, bg, x, y, glyph_width, glyph_height);
                 }
-                texture = SDL_CreateTextureFromSurface(renderer, surface);
-                assert(texture);
-                width = surface->w;
-                height = surface->h;
-                SDL_FreeSurface(surface);
-                SDL_Rect rect = {(int)x * glyph_width, (int)y * glyph_height, width, height};
-                SDL_RenderCopy(renderer, texture, nullptr, &rect);
             }
         }
+        if (!cursor_rendered) render_glyph(renderer, font, ' ', bg, fg, cursor.offset, cursor.line, glyph_width, glyph_height);
+        SDL_SetRenderTarget(renderer, nullptr);
+        SDL_Rect r = {0,0,SCREEN_WIDTH,SCREEN_HEIGHT};
+        SDL_RenderCopy(renderer, texture, nullptr, &r);
         SDL_RenderPresent(renderer);
-
+        
         /*
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, text_texture, NULL, &text_rect);
